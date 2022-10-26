c48205b8f9c913ec9715bb1aeee4a164692aa20f
Aishat-Akinyemi <aishatadenike01@gmail.com>
2022-10-26 15:47:17 +0100

    javascript tests

--- tests/alpha.test.js
+++ tests/alpha.test.js
@@ -4,10 +4,13 @@ describe('alpha contract', () => {
   // Optional timeout. Default is 15000
   jest.setTimeout(30 * 1000)
 
-  const MSG_INIT = { count: 13 }
-  const MSG_INCREMENT = { increment: {} }
-  const MSG_RESET = { reset: { count: 1 } }
-  const QUERY_GET_COUNT = { get_count: {} }
+  const MSG_INIT = { text: 'hello!' }
+  const MSG_RESPOND = { respond: {response: 'hi'} }
+  const MSG_RESET = { reset: { text:  'hey'} }
+  const QUERY_GREETING = { get_greeting : {}}
+  const QUERY_REPLIES = { get_replies: {} }
+
+ 
 
   let alice, bob, contract
 
@@ -17,19 +20,35 @@ describe('alpha contract', () => {
     await contract.deploy(MSG_INIT, 'alpha', { signer: bob })
   })
 
-  test('increment count', async () => {
-    await contract.execute(MSG_INCREMENT, alice)
-    return expect(contract.query(QUERY_GET_COUNT))
-      .resolves.toEqual({ count: 14 })
+  test('query greeting', async () => {
+    return expect(contract.query(QUERY_GREETING))
+      .resolves.toEqual('hello!')
   })
 
-  test('reset count from owner', async () => {
+  test('respond to greeting', async () => {
+    await contract.execute(MSG_RESPOND, alice)
+    let expectedReplies = {
+      replies: [
+        {
+          addr: alice.address,
+          reply: {
+          text: 'hi'
+         }
+        }
+      ]
+    };
+    return expect(contract.query(QUERY_REPLIES))
+      .resolves.toEqual(expectedReplies)
+  
+  })
+
+  test('reset greeting from owner', async () => {
     await contract.execute(MSG_RESET, bob)
-    return expect(contract.query(QUERY_GET_COUNT))
-      .resolves.toEqual({ count: 1 })
+    return expect(contract.query(QUERY_GREETING))
+    .resolves.toEqual('hey')
   })
 
-  test('reset count from user throws unauthorized', () => {
+  test('reset greeting from unauthorised user throws unauthorized', () => {
     return expect(contract.execute(MSG_RESET, alice))
       .rejects.toThrow('Unauthorized')
   })
