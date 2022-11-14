5cb250c792e91bee1940f8ce7e95f0b9f9c391c9
Aishat-Akinyemi <aishatadenike01@gmail.com>
2022-11-15 00:12:48 +0100

    rename directories and files

--- contracts/messaging/examples/schema.rs
+++ contracts/messaging/examples/schema.rs
@@ -3,8 +3,8 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use alpha::msg::{ReplyResponse, ExecuteMsg, InstantiateMsg, QueryMsg};
-use alpha::state::{Config, Reply};
+use messaging::msg::{ReplyResponse, ExecuteMsg, InstantiateMsg, QueryMsg};
+use messaging::state::{Config, Reply};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,10 +2,11 @@ const bre = require('cudos-blast')
 
 async function main() {
   const [alice] = await bre.getSigners()
-  const contract = await bre.getContractFactory('alpha')
+  
+  const contract = await bre.getContractFactory('messaging')
 
   const MSG_INIT = { text: 'hello world!' }
-  await contract.deploy(MSG_INIT, 'alpha', { signer: alice })
+  await contract.deploy(MSG_INIT, 'messaging', { signer: alice })
   console.log(`Contract deployed at: ${contract.getAddress()}`)
 }
 
